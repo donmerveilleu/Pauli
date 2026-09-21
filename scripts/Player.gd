bf50dc789a9ff0d1eeaaa7efca0f02cff11d7aa2
Eyerime <etcubanrenz@gmail.com>
2026-09-22 00:09:45 +0800

    Added idle animation, solidified character movement

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,11 +1,11 @@
 extends CharacterBody2D
 
-#iedit lang diri if di mo ganahan sa speed and grav
 const SPEED = 300.0
 const GRAVITY = 980.0
 const JUMP_VELOCITY = -400.0
 
 @onready var stamina_bar = $UI/StaminaBar
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 var max_stamina = 100.0
 var current_stamina = 100.0
@@ -13,27 +13,23 @@ var stamina_drain_rate = 30.0
 var stamina_regen_rate = 15.0
 
 func _physics_process(delta: float) -> void:
-
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta
 
-	# Handle Jump
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# para ni input
 	var direction := Input.get_axis("move_left", "move_right")
-	
+
 	var current_speed = SPEED
 	var is_sprinting = Input.is_action_pressed("sprint") and direction != 0
-	
-	#machange diri kung pila ang speed sa sprint
+
 	if is_sprinting and current_stamina > 0:
 		current_speed *= 2.0
 		current_stamina -= stamina_drain_rate * delta
 	else:
 		current_stamina += stamina_regen_rate * delta
-		
+
 	current_stamina = clamp(current_stamina, 0.0, max_stamina)
 	stamina_bar.value = current_stamina
 
@@ -43,3 +39,30 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, current_speed)
 
 	move_and_slide()
+
+	update_animation(direction, is_sprinting)
+
+
+func update_animation(direction: float, is_sprinting: bool) -> void:
+	if direction != 0:
+		animated_sprite.flip_h = direction < 0
+
+	if not is_on_floor():
+		if animated_sprite.sprite_frames.has_animation("Jump"):
+			animated_sprite.play("Jump")
+		return
+
+	if direction == 0:
+		animated_sprite.play("Idle")
+		return
+
+	if is_sprinting and current_stamina > 0:
+		if animated_sprite.sprite_frames.has_animation("Run"):
+			animated_sprite.play("Run")
+		else:
+			animated_sprite.play("Walk")
+	else:
+		if animated_sprite.sprite_frames.has_animation("Walk"):
+			animated_sprite.play("Walk")
+		else:
+			animated_sprite.play("Idle")
