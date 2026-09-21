3a87f913e21ac1557746e9df7b965f3d51416897
Eyerime <etcubanrenz@gmail.com>
2026-09-21 22:34:15 +0800

    Init

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,7 +1,9 @@
 extends CharacterBody2D
+
 #iedit lang diri if di mo ganahan sa speed and grav
 const SPEED = 300.0
 const GRAVITY = 980.0
+const JUMP_VELOCITY = -400.0
 
 @onready var stamina_bar = $UI/StaminaBar
 
@@ -15,11 +17,16 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta
 
+	# Handle Jump
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
 	# para ni input
 	var direction := Input.get_axis("move_left", "move_right")
 	
 	var current_speed = SPEED
 	var is_sprinting = Input.is_action_pressed("sprint") and direction != 0
+	
 	#machange diri kung pila ang speed sa sprint
 	if is_sprinting and current_stamina > 0:
 		current_speed *= 2.0
