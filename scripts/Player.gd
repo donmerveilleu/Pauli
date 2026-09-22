9a72383b8f2c447db9f857ce22cdd53978f08daf
Carlos James De Leon <carlosjames.0430@gmail.com>
2026-09-22 16:09:06 +0800

    made W(jump) continuous (legit)

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -16,7 +16,7 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta
 
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	var direction := Input.get_axis("move_left", "move_right")
